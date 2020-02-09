2bba1c8dc57ced63ac49afbd5007ae55f2311eb9
guy <guy@shitchell.com>
2020-02-09 02:50:01 -0500

    added some raw html lameness

---
+++ lastminute/js/vanta.config.js
@@ -0,0 +1,12 @@
+VANTA.FOG({
+	el: "#page-background",
+	mouseControls: true,
+	touchControls: true,
+	minHeight: 200.00,
+	minWidth: 200.00,
+	highlightColor: 0xff00ff,
+	midtoneColor: 0xd77b6e,
+	lowlightColor: 0x208593,
+	baseColor: 0x181616,
+	speed: 0.40
+})
